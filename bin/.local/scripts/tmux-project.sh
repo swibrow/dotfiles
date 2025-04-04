c433b556f06d5a8ef0cb19203dfd8b63fa0e43c2
Samuel <15628653+swibrow@users.noreply.github.com>
2025-04-04 07:53:45 +0200

    add cd to project script

---
+++ bin/.local/scripts/tmux-project.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env zsh
+
+selected_dir=$(cd ~/dev; fd -t d -d 2 . --color=always | fzf --ansi --highlight-line -e)
+if [[ -n "$selected_dir" ]]; then
+    dir_basename=$(basename "$selected_dir")
+    tmux new-window -a -n "$dir_basename" -c "$HOME/dev/$selected_dir" -k
+else
+    echo "No directory selected"
+fi
